3d882cc2d58622d24aefa36d9202ecf4ce0a6f17
Mazharul <mazharul.islam@durbar.live>
2024-01-27 16:02:08 +0600

    pascalsTriangle

---
+++ PascalsTriangle.php
@@ -0,0 +1,15 @@
+<?php
+
+class PascalsTriangle
+{
+    public function generate($numRows)
+    {
+        $PT_array = [];
+        for ($i = 1; $i <= $numRows; $i++) {
+
+        }
+    }
+}
+
+$obj = new PascalsTriangle();
+$obj->generate(3);
